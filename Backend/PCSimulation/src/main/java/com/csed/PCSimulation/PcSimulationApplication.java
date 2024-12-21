d892e84a57d09c7197017b110c28fbec26a7d4db
SamaaIbrahim <es-samaa.ibrahim2027@alexu.edu.eg>
2024-12-21 02:01:36 +0200

    Backend created

---
+++ Backend/PCSimulation/src/main/java/com/csed/PCSimulation/PcSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.csed.PCSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcSimulationApplication.class, args);
+	}
+
+}
---
+++ Backend/PCSimulation/src/test/java/com/csed/PCSimulation/PcSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csed.PCSimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
