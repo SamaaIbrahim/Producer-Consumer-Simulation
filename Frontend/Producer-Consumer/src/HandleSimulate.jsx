9d0dfcce6fb3081ce200d88389b08e1d682773e0
Ahmed-Ehab-601 <medo.losy@gmail.com>
2025-01-05 15:43:06 +0200

    edit

--- Frontend/Producer-Consumer/src/HandleSimulate.jsx
+++ Frontend/Producer-Consumer/src/HandleSimulate.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
-function HandleSimulate(nodes,edges,numberOfProducts){
+async function HandleSimulate(nodes,edges,numberOfProducts){
     const machines = nodes.filter((node)=> node.type === "machine").map((machine)=> ({id : machine.id,inQueuesIds:[],outQueueId:""}))
-    const queues = nodes.filter((node)=> node.type === "queue").map((queue)=>({id : queue.id , isStart:queue.id==="StartQ",isEnd:queue.id==="End`queue"}))
+    const queues = nodes.filter((node)=> node.type === "queue").map((queue)=>({id : queue.id , isStart:queue.id==="StartQ",isEnd:queue.id==="EndQ"}))
 
 
     edges.forEach((edge) => {
         const { source, target } = edge;
     
-        if (source.startsWith("M") && target.startsWith("Q")) {
+        if (source.startsWith("M") && (target.startsWith("Q") || target.startsWith("EndQ")) ) {
             const machine = machines.find((m) => m.id === source);
             if (machine) {
                 machine.outQueueId = target;
@@ -27,21 +27,17 @@ function HandleSimulate(nodes,edges,numberOfProducts){
     console.log(queues)
     console.log(numberOfProducts)
 
-    async () => {
-        
-        console.log("hello")
+
+       
         try{
-            const response = await axios.post("http://localhost:8080/simulate/",simulationDto);
+            const response = await axios.post(`http://localhost:8080/simulate`,simulationDto);
             if(response.status === 200){
                 console.log("done")
             }
 
         }catch(error){
             console.log(error.response.data)
-        }
-
-    }
-  
+        }  
     
 }
 export default HandleSimulate;
